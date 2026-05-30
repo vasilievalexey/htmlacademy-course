7f524fd7af46f085251e41006f853c02afadc96f
vasilievalexey <vasilievalexey@mail.ru>
2026-05-30 09:51:40 -0400

    Add comments pagination

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,10 @@ const commentsList = document.querySelector('.social__comments');
 const commentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 
+const COMMENTS_PER_PAGE = 5;
+let currentComments = [];
+let shownCount = 0;
+
 const createCommentElement = ({ avatar, name, message }) => {
   const li = document.createElement('li');
   li.classList.add('social__comment');
@@ -15,10 +19,25 @@ const createCommentElement = ({ avatar, name, message }) => {
   return li;
 };
 
-const onDocumentKeydown = (evt) => {
-  if (evt.key === 'Escape') {
-    closeBigPicture();
+const renderComments = () => {
+  const nextComments = currentComments.slice(shownCount, shownCount + COMMENTS_PER_PAGE);
+  const fragment = document.createDocumentFragment();
+  nextComments.forEach((comment) => fragment.appendChild(createCommentElement(comment)));
+  commentsList.appendChild(fragment);
+
+  shownCount += nextComments.length;
+
+  commentCount.textContent = `${shownCount} из ${currentComments.length} комментариев`;
+
+  if (shownCount >= currentComments.length) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
   }
+};
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape') closeBigPicture();
 };
 
 function openBigPicture(picture) {
@@ -31,15 +50,13 @@ function openBigPicture(picture) {
   bigPicture.querySelector('.social__caption').textContent = picture.description;
 
   commentsList.innerHTML = '';
-  const fragment = document.createDocumentFragment();
-  picture.comments.forEach((comment) => {
-    fragment.appendChild(createCommentElement(comment));
-  });
-  commentsList.appendChild(fragment);
+  currentComments = picture.comments;
+  shownCount = 0;
 
-  commentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  commentCount.classList.remove('hidden');
+  renderComments();
 
+  commentsLoader.addEventListener('click', renderComments);
   cancelButton.addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', onDocumentKeydown);
 }
@@ -48,6 +65,7 @@ function closeBigPicture() {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
 
+  commentsLoader.removeEventListener('click', renderComments);
   cancelButton.removeEventListener('click', closeBigPicture);
   document.removeEventListener('keydown', onDocumentKeydown);
 }
